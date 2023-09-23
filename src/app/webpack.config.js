fcb500b7fe098afaded9ad572f6c112fa283a119
Maksim C <jstars.tech@gmail.com>
2023-09-23 23:12:29 +0300

    Refactor: build dont extract comments

--- src/app/webpack.config.js
+++ src/app/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     mode: 'development',
@@ -24,6 +24,15 @@ module.exports = {
                 secure: false
             }
         }
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+              extractComments: false,
+            }),
+          ],
     },
 
     module: {
